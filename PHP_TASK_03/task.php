c7b6b44a85fe780add940782d7ed180805fc1125
NiloyGhosh6351 <62530599+NiloyGhosh6351@users.noreply.github.com>
2020-07-23 19:12:14 +0600

    name

--- PHP_TASK_03/task.php
+++ PHP_TASK_03/task.php
@@ -1,8 +1,49 @@
 <?php
 
-if (isset($_GET['submit'])) {
+if (isset($_GET['name'])) {
 	$a=$_GET['name'];
-	if ($a== "") {
+	
+	if (strlen($a)>0) {
+		# code...
+		if (str_word_count($a)>1) {
+			# code....
+			//$a=range(a, z)
+			//$a=range(A, Z)
+			if(($a[0]>='a' && $a[0]<='z') || ($a[0]>='A' && $a[0]<='Z')){
+				$b=strlen($a);
+				while ($b<0) {
+					# code...
+					if ($a[$b]<'a' && $a[$b]!='' && $a[$b]!='.' && $a[$b]!='-') {
+						# code...
+						echo "invalid";
+						break;
+					}
+					elseif ($a[$b]>'Z') {
+						# code...
+						if ($a[$b]<'a') {
+							# code...
+							echo "Invalid";
+							break;
+						}
+						elseif ($a[$b]<'a') {
+							# code...
+							echo "Invalid";
+							break;
+						}
+					}
+					$b=$b-1;
+
+				}
+			}
+			else{ echo "Please input a valid name";}
+		}
+		else{ echo "Please input a valid name";}
+	}
+	else{ echo "Please input a valid name";}
+
+
+
+	/*if ($a== "") {
 		echo "Cannot be empty";
 	}
 	elseif (strlen($a)) {
@@ -13,7 +54,7 @@ if (isset($_GET['submit'])) {
 	}
 	elseif ($b>= 'a'&& $b<='z' || $b>= 'A'&& $b<='Z' || $b>='.' || $b>='-') {
 		echo "Contain a-z or A-Z or dot(.) or dash(-)";
-	}
+	}*/
 }
 
 
@@ -21,13 +62,12 @@ if (isset($_GET['submit'])) {
 
 
 
-<!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-	<form action="#" method="GET">
+	<form>
 	<h3>Name</h3>
 	<input type="text" name="name"><br>
 	<br>
