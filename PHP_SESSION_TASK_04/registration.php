46efd885f6e053a57e3f3af36c0efdaab5781188
NiloyGhosh6351 <62530599+NiloyGhosh6351@users.noreply.github.com>
2020-07-28 22:15:58 +0600

    registration

--- PHP_SESSION_TASK_04/registration.php
+++ PHP_SESSION_TASK_04/registration.php
@@ -2,31 +2,36 @@
 
 	if(isset($_POST['submit'])){
 
-		$name = $_POST['username'];
+		$name = $_POST['name'];
 		$email = $_POST['email'];
+		$username = $_POST['username'];
 		$password = $_POST['password'];
+		$confirmpassword = $_POST['confirmpassword'];
+		$gender = $_POST['gender'];
+		$day = $_POST['day'];
+		$month = $_POST['month'];
+		$year = $_POST['year'];
 
-		$enc = md5($password);
-
-		if(empty($name) || empty($password) || empty($email)){
-			echo "null submission";
+		if(empty($name) || empty($email) || empty($username) || empty($password) || empty($confirmpassword) || empty($gender) || empty($day) || empty($month) || empty($year)){
+			echo "fillup the table";
 		}else {
-
-			$user = [
-						'name'=>$name,
-						'email'=>$email,
-						'password'=>$password
-					];
 
 			setcookie('name', $name, time()+3600, '/');
 			setcookie('email', $email, time()+3600, '/');
-			setcookie('password', $enc, time()+3600, '/');
+			setcookie('username', $username, time()+3600, '/');
+			setcookie('password', $password, time()+3600, '/');
+			setcookie('confirmpassword', $confirmpassword, time()+3600, '/');
+			setcookie('gender', $gender, time()+3600, '/');
+			setcookie('day', $day, time()+3600, '/');
+			setcookie('month', $month, time()+3600, '/');
+			setcookie('year', $year, time()+3600, '/');
 
-			header('location: .html');
+			header('location: home.html');
 		}
 
 	}else{
-		header("location: .html");
+		header("location: login.html");
+		//echo "invalid";
 	}
 
 
