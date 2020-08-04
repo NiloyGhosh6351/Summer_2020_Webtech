f1cb0cde4441fe95a9593e36b65b802b9c475517
NiloyGhosh6351 <62530599+NiloyGhosh6351@users.noreply.github.com>
2020-08-04 08:56:04 +0600

    registration login

---
+++ Mid_Lab_Exam/login.php
@@ -0,0 +1,37 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$id 		= $_POST['id'];
+		$password 	= $_POST['password'];
+
+		if(empty($id) || empty($password)){
+			echo "null submission";
+
+		}else{
+			
+			$file = fopen('registration.txt', 'r');
+			$data = fread($file, filesize('registration.txt'));
+			$user = explode('|', $data);
+
+
+			/*while(!feof($data)){
+				$user = fgets($data);
+				$user = explode('|', $data);
+			}*/
+
+			//print_r($user);
+
+			if(trim($user[0]) == $id && trim($user[1]) == $password){
+				$_SESSION['status']  = "Ok";
+				header('location: home.php');
+			}else{
+				echo "Invalid username/password";
+			}
+		}
+
+	}else{
+		header("location: login.html");
+	}
+
+
+?>
